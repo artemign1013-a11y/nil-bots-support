ff33d5651f03898e0a2420a566a556707965d7f2
artemign1013-a11y <artemign1013@gmail.com>
2026-09-14 12:38:41 +0330

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,9 @@
-# Токен бота от BotFather
-BOT_TOKEN = "8650697625:AAH8IWfbP_7o7kziD2aemyIPGak9l7JdVCY"
-# Firebase конфиг (тот же, что на сайте)
+
+import os
+
+BOT_TOKEN = os.environ.get("8650697625:AAH8IWfbP_7o7kziD2aemyIPGak9l7JdVCY")
+ADMIN_ID = int(os.environ.get("5244755472"))
+
 FIREBASE_CONFIG = {
     "apiKey": "AIzaSyBfx3lX593cjF2N2Aq9WcEK2Fn_pie3IT8",
     "authDomain": "tg-bots-3ceaf.firebaseapp.com",
@@ -9,7 +12,3 @@ FIREBASE_CONFIG = {
     "messagingSenderId": "737012691164",
     "appId": "1:737012691164:web:e2aea0c41bb6e09ad30419",
 }
-
-# Твой Telegram ID (чтобы бот знал, кто админ)
-# Узнай через @userinfobot
-ADMIN_ID = 5244755472  # ЗАМЕНИ НА СВОЙ
