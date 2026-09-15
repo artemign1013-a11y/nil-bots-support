b8149ac571cda61737046d8d1ec82f170f1c61f7
artemign1013-a11y <artemign1013@gmail.com>
2026-09-15 12:47:00 +0330

    Update config.py

--- config.py
+++ config.py
@@ -1,13 +1,1 @@
-import os
 
-BOT_TOKEN = os.environ.get("BOT_TOKEN")
-ADMIN_ID = int(os.environ.get("ADMIN_ID"))
-
-FIREBASE_CONFIG = {
-    "apiKey": "AIzaSyBfx3lX593cjF2N2Aq9WcEK2Fn_pie3IT8",
-    "authDomain": "tg-bots-3ceaf.firebaseapp.com",
-    "projectId": "tg-bots-3ceaf",
-    "storageBucket": "tg-bots-3ceaf.firebasestorage.app",
-    "messagingSenderId": "737012691164",
-    "appId": "1:737012691164:web:e2aea0c41bb6e09ad30419",
-}
